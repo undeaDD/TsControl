dd3c42e2f3f0eca72920c99cc0f2cc72ff38a3b6
Dominic Drees <dominic@undeaD-MAC.local>
2019-01-03 02:15:13 +0100

    first ui implementations

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,13 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setAttribute(Qt::WA_TranslucentBackground, true);
+
+    ui->listWidget->addItem(new QListWidgetItem(QIcon(":/icon/teamspeak"), "1"));
+    ui->listWidget->addItem(new QListWidgetItem(QIcon(":/icon/teamspeak"), "2"));
+    ui->listWidget->addItem(new QListWidgetItem(QIcon(":/icon/teamspeak"), "3"));
+    ui->listWidget->addItem(new QListWidgetItem(QIcon(":/icon/teamspeak"), "4"));
 }
 
 MainWindow::~MainWindow()
