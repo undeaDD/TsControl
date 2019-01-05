2aa3f4c12832e14bdde4d133d88d9abe19040e58
Dominic Drees <dominic@undeaD-MAC.local>
2019-01-05 22:22:38 +0100

    cleanup v1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,21 +1,12 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    ui->listWidget->addItem(new QListWidgetItem());
 
     setAttribute(Qt::WA_TranslucentBackground, true);
-
-    ui->listWidget->addItem(new QListWidgetItem(QIcon(":/icon/teamspeak"), "1"));
-    ui->listWidget->addItem(new QListWidgetItem(QIcon(":/icon/teamspeak"), "2"));
-    ui->listWidget->addItem(new QListWidgetItem(QIcon(":/icon/teamspeak"), "3"));
-    ui->listWidget->addItem(new QListWidgetItem(QIcon(":/icon/teamspeak"), "4"));
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
